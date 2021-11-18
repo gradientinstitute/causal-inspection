052a19d925da553b8c217b243a293ecf7a74fa9a
Dan Steinberg <dan.m.steinberg@gmail.com>
2021-11-18 15:42:18 +1100

    statistics for linear regression

--- cinspect/estimators.py
+++ cinspect/estimators.py
@@ -1,14 +1,87 @@
 """
 Some convenience estimators for causal estimation.
+
 Copyright (C) 2019-2021 Gradient Institute Ltd.
 """
 
 import numpy as np
 import pandas as pd
 
+from typing import NamedTuple, Union
 from multimethod import multimethod
+from scipy import linalg
+from scipy import stats
+
 from sklearn.base import BaseEstimator, RegressorMixin, clone
 from sklearn.utils.validation import check_is_fitted
+from sklearn.linear_model import LinearRegression
+
+
+class RegressionStatisticalResults(NamedTuple):
+    """Statistical results object for linear regressors.
+
+    Attributes
+    ----------
+    beta: float or ndarray
+        the regression coefficients
+    std_err: float or ndarray
+        The standard error of the coefficients
+    t_stat: float or ndarray
+        The t-statistics for the regression coefficients
+    p_value: float or ndarray
+        The p-value of the two-sided t-test on the coefficients. The null
+        hypothesis is that beta = 0, and the alternate hypothesis is beta != 0.
+    dof: float
+        The degrees of freedom used to compute the t-test.
+    """
+
+    beta: Union[float, np.ndarray]
+    std_err: Union[float, np.ndarray]
+    t_stat: Union[float, np.ndarray]
+    p_value: Union[float, np.ndarray]
+    dof: float
+
+    def __repr__(self) -> str:
+        """Return string representation of StatisticalResults."""
+        reprs = f"""Statistical results:
+            beta =
+                {self.beta},
+            s.e.(beta) =
+                {self.std_err}
+            t-statistic(s):
+                {self.t_stat}
+            p-value(s):
+                {self.p_value}
+            Degrees of freedom: {self.dof}
+            """
+        return reprs
+
+
+class LinearRegressionStat(LinearRegression):
+    """Scikit learn's LinearRegression estimator with coefficient stats."""
+
+    def fit(self, X, y, sample_weight=None):
+        super().fit(X, y, sample_weight)
+        n, d = X.shape
+        self.dof_ = n - d
+        shp = (d,) if np.isscalar(self._residues) else (d, len(self._residues))
+        s2 = ((self._residues / self.dof_) * np.ones(shp)).T
+        self.coef_se_ = np.sqrt(linalg.pinv(X.T @ X).diagonal() * s2)
+        self.t_ = self.coef_ / self.coef_se_
+        self.p_ = (1. - stats.t.cdf(np.abs(self.t_), df=self.dof_)) * 2
+        return self
+
+    def model_statistics(self):
+        """Get the coefficient statistics for this estimator."""
+        check_is_fitted(self, attributes=["coef_", "coef_se_"])
+        stats = RegressionStatisticalResults(
+            beta=self.coef_,
+            std_err=self.coef_se_,
+            dof=self.dof_,
+            t_stat=self.t_,
+            p_value=self.p_
+        )
+        return stats
 
 
 class BinaryTreatmentRegressor(BaseEstimator, RegressorMixin):
@@ -106,6 +179,7 @@ def _treatment_split(X, t_col, t_val):
     Xt = X[t_mask]
     Xc = X[~t_mask]
     return Xt, Xc, t_mask
+
 
 
 @multimethod
