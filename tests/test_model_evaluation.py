447d281574e340e832e8046e31e2f197d9e8d840
Jack Fitzgerald Sice <12495594+jack-fs@users.noreply.github.com>
2022-02-23 12:29:48 +1100

    Test reproducibility of model evaluators

--- tests/test_model_evaluation.py
+++ tests/test_model_evaluation.py
@@ -72,3 +72,48 @@ def test_eval_function_calls(eval_func):
 
     evaluators = eval_func(estimator, X, y, evaluators)
     assert evaluators[0].aggregate_call  # type: ignore
+
+class RandomEvaluator(Evaluator):
+    def __init__(self):
+        self.random_state = None
+        self.results = None
+        pass
+    def prepare(self, estimator, X, y=None, random_state=None):
+        self.random_state=random_state
+    def _evaluate(self, estimator, X, y=None):
+        rng = self._get_rng()
+        result = rng.random()
+        return result
+
+    evaluate_all = _evaluate
+    evaluate_train = _evaluate
+    evaluate_test = _evaluate
+
+    def get_results(self):
+        return self.results
+
+    def _get_rng(self):
+        """
+        Get our rng; get stored rng/seed new one.
+        If we have no rng, make a random one.
+        """
+        if self.random_state is None:
+             rng = np.random.default_rng()
+        elif isinstance(self.random_state, int):
+             rng = np.random_default_rng(self.random_state)
+        else: 
+             rng = self.random_state
+        return rng
+
+@pytest.mark.parametrize("eval_func", [crossval_model, bootstrap_model])
+def test_reproducible_function_calls(eval_func):
+    """Test that model evaluator functions produce same output given same input"""
+    estimator = _MockEstimator()
+    evaluators = [RandomEvaluator()]
+    random_state = 42
+
+    X, y = pd.DataFrame(np.ones((100, 2))), pd.Series(np.ones(100))
+    evaluators_1 = eval_func(estimator,X,y,evaluators, random_state=random_state)
+    evaluators_2 = eval_func(estimator,X,y,evaluators, random_state=random_state)
+    assert evaluators_1 == evaluators_2
+
