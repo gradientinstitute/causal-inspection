4c2655d4ecbb19442b0d1e1216b05b1b13b57c73
Jack Fitzgerald Sice <12495594+jack-fs@users.noreply.github.com>
2022-03-16 11:32:36 +1100

    Linting

--- tests/test_sims.py
+++ tests/test_sims.py
@@ -1,5 +1,5 @@
-"""Simulations tests"""
-import hypothesis as hyp
+"""Simulations tests."""
+
 import numpy as np
 from hypothesis import given
 from hypothesis import strategies as st
@@ -7,15 +7,15 @@ from simulations import collinear_sim, simple_sim
 
 
 @st.composite
-def n_x_support_strat(draw):
+def _n_x_support_strat(draw):
     n_x = draw(st.integers(min_value=0, max_value=10))
     support = draw(st.integers(min_value=0, max_value=n_x))
     return (n_x, support)
 
 
 @given(
-    n_x=st.shared(n_x_support_strat(), key="nxs").map(lambda tup: tup[0]),
-    support_size=st.shared(n_x_support_strat(), key="nxs").map(lambda tup: tup[1]),
+    n_x=st.shared(_n_x_support_strat(), key="nxs").map(lambda tup: tup[0]),
+    support_size=st.shared(_n_x_support_strat(), key="nxs").map(lambda tup: tup[1]),
     random_state=st.one_of(st.integers(min_value=0, max_value=2**32 - 1)),
 )
 def test_simple_data_generation_deterministic(n_x, support_size, random_state):
