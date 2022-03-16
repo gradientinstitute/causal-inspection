8189e9ff87de8b0fe517ac5318c0eab4f06f291a
Jack Fitzgerald Sice <12495594+jack-fs@users.noreply.github.com>
2022-03-16 16:18:29 +1100

    Linting, parametrise collinear_sim by ATE

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,6 @@
 """Testing utilities."""
 
-from typing import Callable, Iterable, Sequence, TypeVar, Union
+from typing import Callable, Iterable, Optional, Sequence, TypeVar, Union
 
 import numpy as np
 import numpy.typing as npt
@@ -12,8 +12,27 @@ def draw_bootstrap_samples(
     values: npt.ArrayLike,
     sample_size: npt._ShapeLike,
     n_repeats: int,
-    random_seed: Union[None, int, np.random.RandomState] = None,
+    random_seed: Optional[Union[int, np.random.RandomState]] = None,
 ) -> Iterable[np.ndarray]:
+    """
+    Draw random samples from given values with replacement
+
+    Parameters
+    ----------
+    values : npt.ArrayLike
+        Values to bootstrap from
+    sample_size : npt._ShapeLike
+        Size/shape of bootstrap batches
+    n_repeats : int
+        number of bootstrap batches to generate
+    random_seed : Optional[Union[int, np.random.RandomState]], optional
+        random seed, by default None
+
+    Returns
+    -------
+    Iterable[np.ndarray]
+        Iterable of bootstrap batches
+    """
     rng = check_random_state(random_seed)
     # rng = np.random.default_rng(rng)
 
@@ -50,15 +69,16 @@ def bootstrap(
         ( f(X[ixs]) for ixs in indices )
     """
 
-    vs = (
-        f(X[ixs]) for ixs in indices
-        )
+    vs = (f(X[ixs]) for ixs in indices)
     return vs
 
 
 def confidence_intervals(
     values: Iterable[float], quantiles: Sequence[float] = (0.005, 0.995)
 ) -> np.ma.MaskedArray:
+    """Return quantiles of given values.
+    
+    Wraps mquantiles for use with general iterables"""
     # breakpoint()
     cis: np.ma.MaskedArray = mquantiles(list(values), quantiles)
     return cis
