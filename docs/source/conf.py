a6f0541b6e639208f024e2b0294ef7adf80c2cb0
Jack Fitzgerald Sice <12495594+jack-fs@users.noreply.github.com>
2023-02-04 10:40:12 +1100

    Add napoleon sphinx extension for numpy docstrings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,9 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.napoleon'  # use numpy docstring format
+              ]
 
 templates_path = ['_templates']
 exclude_patterns = []
