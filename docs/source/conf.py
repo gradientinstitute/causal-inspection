6dc0a626ba4304ddb4b729f8e5566cf49dd07e22
Jack Fitzgerald Sice <12495594+jack-fs@users.noreply.github.com>
2023-02-04 11:40:47 +1100

    Link to external documentation for e.g. sklearn classes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,13 +15,35 @@ release = '0.0.1'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ['sphinx.ext.autodoc',
-              'sphinx.ext.napoleon'  # use numpy docstring format
+              'sphinx.ext.napoleon',  # use numpy docstring format
+              # 'sphinx.ext.coverage',  # -b coverage on cl gives documentation coverage
+              'sphinx.ext.extlinks',
+              #   'sphinx.ext.linkcode',  # for linking to external repo.
+              # for potential use with github,
+              # see https://github.com/readthedocs/sphinx-autoapi/issues/202
+              # or https://github.com/scikit-image/scikit-image/pull/1628
+              'sphinx.ext.viewcode',  # embeds source code in generated html
+              'sphinx.ext.intersphinx',  # link to external docs for dependencies
               ]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
+# -- Options for sphinx.ext.extlinks 
 
+# use :issue:`123` to link to project issue on GitHub
+extlinks = {'issue': ('https://github.com/gradientinstitute/cinspect/issues/%s',
+                      'issue %s')}
+
+
+# -- Options for sphinx.ext.intersphinx 
+
+intersphinx_mapping = {'python': ('https://docs.python.org/3', None),
+                       'scikit-learn': ('https://scikit-learn.org/stable/', None),
+                       'pandas': ('https://pandas.pydata.org/docs/', None),
+                       'numpy': ('https://numpy.org/doc/stable/', None),
+                       'matplotlib': ('https://matplotlib.org/stable/', None),
+}
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
